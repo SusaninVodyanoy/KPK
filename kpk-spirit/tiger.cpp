ff171bad8e76d53e2ee580818e3de1e699ef95c7
SusaninVodyanoy <61546634+SusaninVodyanoy@users.noreply.github.com>
2020-02-29 12:10:01 +0300

    Create structure

--- kpk-spirit/tiger.cpp
+++ kpk-spirit/tiger.cpp
@@ -1,9 +1,28 @@
 #include "TXLib.h"
 
+struct AttributeImg
+{
+int posx, posy;
+int kadrx, kadry;
+HDC catImage;
+};
+
+void animic (AttributeImg ig, int step, int anim);
+
+
+// POINT sz = txGetExtent();
 int main()
     {
+    AttributeImg s1 = {100, 100, 3, 4, txLoadImage ("cat-drawing-film.bmp")};
+    AttributeImg s2 = {600, 100, 6, 2, txLoadImage ("cat-run-cycle.bmp")};
+    AttributeImg s3 = {100, 400, 5, 4, txLoadImage ("leopad.bmp")};
+    AttributeImg s4 = {600, 400, 6, 1, txLoadImage ("tiger-game-sprites.bmp")};
+
+
+
     txCreateWindow (800, 600);
-    txSetFillColor     (TX_WHITE);
+    txSetColor     (TX_WHITE);
+    txClear;
     //HDC catImage = txLoadImage ("cat-drawing-film.bmp");
     //int framesx = 3;
     //int framesy = 4;
@@ -13,28 +32,40 @@ int main()
     //HDC catImage = txLoadImage ("leopad.bmp");
     //int framesx = 5;
     //int framesy = 4;
-    HDC catImage = txLoadImage ("tiger-game-sprites.bmp");
-    int framesx = 6;
-    int framesy = 1;
+    //HDC catImage = txLoadImage ("tiger-game-sprites.bmp");
+    //int framesx = 6;
+    //int framesy = 1;
 
 
-    int frameSizex = txGetExtentX (catImage) /framesx;
-    int frameSizey = txGetExtentY (catImage) /framesy;
+
 
     int anim = 0;
-
-    for (int t = 0; !txGetAsyncKeyState (VK_ESCAPE); t++)
+    for (int t = 0; !txGetAsyncKeyState (VK_ESCAPE); t++, anim)
         {
-        txBitBlt (txDC(), 100, 100, frameSizex, frameSizey,
-                  catImage,
-                  (t % framesx) * frameSizex, anim * frameSizey);
-        if (txGetAsyncKeyState ('1')) anim = 0;
-        if (txGetAsyncKeyState ('2')) anim = 1;
-        if (txGetAsyncKeyState ('3')) anim = 2;
-        if (txGetAsyncKeyState ('4')) anim = 3;
+        animic (s1,t,anim);
+        animic (s2,t,anim);
+        animic (s3,t,anim);
+        animic (s4,t,anim);
+        if (txGetAsyncKeyState (VK_LEFT))  anim--;
+        if (txGetAsyncKeyState (VK_RIGHT)) anim++;
         txSleep (80);
         }
 
-    txDeleteDC (catImage);
+    txDeleteDC (s1.catImage);
+    txDeleteDC (s2.catImage);
+    txDeleteDC (s3.catImage);
+    txDeleteDC (s4.catImage);
     return 0;
+    }
+
+void animic (AttributeImg ig, int step, int anim)
+    {
+
+    int frameSizex = txGetExtentX (ig.catImage) /ig.kadrx;
+    int frameSizey = txGetExtentY (ig.catImage) /ig.kadry;
+    txBitBlt (txDC(), ig.posx, ig.posy, frameSizex, frameSizey,
+              ig.catImage,
+              (step % ig.kadrx) * frameSizex, anim * frameSizey);
+
+
     }
