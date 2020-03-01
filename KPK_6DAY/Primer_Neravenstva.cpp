06236593c3ac850b8070f81abbbf1f1b4a0c5ff6
SusaninVodyanoy <61546634+SusaninVodyanoy@users.noreply.github.com>
2020-03-01 16:06:56 +0300

    Draw Cat

--- KPK_6DAY/Primer_Neravenstva.cpp
+++ KPK_6DAY/Primer_Neravenstva.cpp
@@ -1,18 +1,27 @@
 
 #include "TXLib.h"
 
-void DrawParabola ();
-void DrawCircle ();
+
+void DrawCircle (double x0, double y0, COLORREF color, double xx, double yy, double radius);
+void Drawears   (double x0, double y0, COLORREF color, double xx, double yy, double radius);
 void DrawRomb ();
+void DrawParabola ();
+void DrawPupils (double x0, double y0, COLORREF color, double xx, double yy);
+void Drawmoustache ();
 
 int main()
     {
     txCreateWindow (800, 600);
 
-    DrawParabola ();
-    DrawCircle ();
-    DrawRomb ();
-
+    DrawParabola    (                                   );
+    DrawCircle      (0, 0, TX_ORANGE,         1,  1,   5);
+    DrawCircle      (0, 0, TX_LIGHTBLUE,    -50, 30, 0.2);
+    DrawCircle      (0, 0, TX_LIGHTBLUE,     50, 30, 0.2);
+    DrawRomb        (                                   );
+    Drawears        (0, 0, TX_BROWN,          1,  1,   5);
+    Drawmoustache   (                                   );
+    DrawPupils      (0, 0, TX_YELLOW,       -45, 30     );
+    DrawPupils      (0, 0, TX_YELLOW,        55, 30     );
     return 0;
     }
 
@@ -22,31 +31,63 @@ void DrawParabola ()
     for (double y = -10; y<=10; y += 0.04)
     for (double x = -10; x<=10; x += 0.04)
         {
-         if (y>x*x)
-            txSetPixel (400+50*x, 300 - 50*y,TX_LIGHTRED);
+         if (y>2*x*x && y<x*x+0.3)
+            txSetPixel (400+50*x, 380 - 50*y,TX_LIGHTRED);
         }
     }
 
-void DrawCircle ()
+void DrawCircle (double x0, double y0, COLORREF color, double xx, double yy, double radius)
     {
 
     for (double y = -10; y<=10; y += 0.04)
     for (double x = -10; x<=10; x += 0.04)
         {
-         if (x*x+y*y<=4)
-            txSetPixel (401+50*x, 300 - 50*y,TX_LIGHTGREEN);
+         if ((x+x0)*(x+x0)+(y+y0)*(y+y0)<=radius)
+            txSetPixel (401 + xx + 50*x, 300 - yy - 50*y,color);
 
         }
     }
 
 void DrawRomb ()
     {
+    for (double y = -10; y<=10; y += 0.04)
+    for (double x = -10; x<=10; x += 0.04)
+        {
+         if (fabs (x)+fabs (y)<=2 && y>1.4)
+            txSetPixel (400+50*x, 301 +90 - 50*y,TX_BROWN);
+
+        }
+    }
+
+void Drawears   (double x0, double y0, COLORREF color, double xx, double yy, double radius)
+    {
+    for (double y = -10; y<=10; y += 0.04)
+    for (double x = -10; x<=10; x += 0.04)
+        {
+         if (y < fabs (1.5*(x+x0)+0.3) && (x+x0)*(x+x0)+(y+y0)*(y+y0)>=radius+0.5 && y>0.5 && x<2.2 && x>-2.2)
+            txSetPixel (400+xx+50*x, 301 +yy- 50*y,color);
+
+        }
+    }
+
+
+void Drawmoustache ()
+    {
 
     for (double y = -10; y<=10; y += 0.04)
     for (double x = -10; x<=10; x += 0.04)
         {
-         if (fabs (x)+fabs (y)<=4)
-            txSetPixel (400+50*x, 301 - 50*y,TX_LIGHTBLUE);
-
+         if (((y>-0.1*x+0.01 && y<-0.1*x+0.05) || (y>0.1*x+0.01 && y<0.1*x+0.05 ))&& x<2.5 && x>-2.5 && !(x<1 && x>-1))
+            txSetPixel (400+50*x, 350 - 50*y,TX_LIGHTRED);
         }
     }
+void DrawPupils (double x0, double y0, COLORREF color, double xx, double yy)
+    {
+
+    for (double y = -10; y<=10; y += 0.04)
+    for (double x = -10; x<=10; x += 0.04)
+        {
+         if (y*y<-x*1.4 && y*y<x*1.4+0.5)
+            txSetPixel (401 + xx + 50*x, 300 - yy - 50*y,TX_LIGHTRED);
+        }
+   }
