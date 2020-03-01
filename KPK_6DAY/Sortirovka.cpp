e94bc5f76b8f6af07444b4edd8955387cc20fb81
SusaninVodyanoy <61546634+SusaninVodyanoy@users.noreply.github.com>
2020-03-01 18:07:22 +0300

    Create project sortirovka

---
+++ KPK_6DAY/Sortirovka.cpp
@@ -0,0 +1,114 @@
+#include "TXLib.h"
+
+struct Button
+{
+int posx, posy;
+int sizex, sizey;
+COLORREF corolBody;
+COLORREF corolFrame;
+bool CLick;
+void Draw    () const;
+void OnCLick (int numb);
+};
+
+void SelectWay ();
+
+int main()
+    {
+
+    Button Buttons [7] = {{ 10,10, 100, 50, TX_YELLOW, TX_LIGHTGREEN, false},
+                          {120,10, 100, 50, TX_YELLOW, TX_LIGHTGREEN, false},
+                          {230,10, 100, 50, TX_YELLOW, TX_LIGHTGREEN, false},
+                          {340,10, 100, 50, TX_YELLOW, TX_LIGHTGREEN, false},
+                          {450,10, 100, 50, TX_YELLOW, TX_LIGHTGREEN, false},
+                          {560,10, 100, 50, TX_YELLOW, TX_LIGHTGREEN, false},
+                          {670,10, 100, 50, TX_YELLOW, TX_LIGHTGREEN, false}};
+
+
+    txCreateWindow (800, 600);
+    txTextCursor (false);
+    txBegin ();
+
+    while (!txGetAsyncKeyState (VK_ESCAPE))
+        {
+        txTextOut   (100, 100, "Выберите способ сортировки от 1 до 5");
+        for (int i = 0; i<7; i ++)
+            {
+            Buttons[i].Draw     ();
+            Buttons[i].OnCLick  (i);
+            }
+        SelectWay ();
+
+        txSleep (0);
+
+        }
+
+
+    return 0;
+    }
+
+
+void SelectWay ()
+    {
+    if (txGetAsyncKeyState (0x31))
+        {
+        txSetFillColor (TX_BLACK);
+        txClear     ();
+        txTextOut   (300, 400, "Выбран 1 способ сортировки");
+        }
+
+    if (txGetAsyncKeyState (0x32))
+        {
+        txSetFillColor (TX_BLACK);
+        txClear     ();
+        txTextOut   (300, 400, "Выбран 2 способ сортировки");
+        }
+
+    if (txGetAsyncKeyState (0x33))
+        {
+        txSetFillColor (TX_BLACK);
+        txClear     ();
+        txTextOut   (300, 400, "Выбран 3 способ сортировки");
+        }
+
+    if (txGetAsyncKeyState (0x34))
+        {
+        txSetFillColor (TX_BLACK);
+        txClear     ();
+        txTextOut   (300, 400, "Выбран 4 способ сортировки");
+        }
+    if (txGetAsyncKeyState (0x35))
+        {
+        txSetFillColor (TX_BLACK);
+        txClear     ();
+        txTextOut   (300, 400, "Выбран 5 способ сортировки");
+        }
+    if (txGetAsyncKeyState (VK_SPACE))
+        {
+        txSetFillColor (TX_BLACK);
+        txClear     ();
+        }
+    }
+
+void Button::Draw () const
+    {
+
+    txSetFillColor  (corolBody);
+    txSetColor      (corolFrame);
+    txRectangle     (posx, posy, posx+sizex, posy+sizey);
+
+    }
+
+void Button::OnCLick (int numb)
+    {
+    if (txMouseX()>posx && txMouseX()<posx+sizex && txMouseY()>posy && txMouseY()<posy+sizey)
+        {
+        printf ("Мышь наведена на кнопку %d\n", numb);
+        if (txMouseButtons() & 1)
+            {
+            CLick=true;
+            printf ("Нажата кнопка %d\n", numb);
+            }
+        }
+    }
+
