22ec9bbde8417ba91d44191417933b398dcfd8d1
SusaninVodyanoy <61546634+SusaninVodyanoy@users.noreply.github.com>
2020-03-01 10:23:04 +0300

    сообщение об ошибке и оператор множественного выбора

--- KPK_6DAY/Primer.cpp
+++ KPK_6DAY/Primer.cpp
@@ -1,4 +1,7 @@
 #include "TXLib.h"
+const int SIN = 1,
+          SQR = 2;
+
 void DrawGraphic (int func, COLORREF color);
 double Sqr (double x);
 
@@ -6,8 +9,8 @@ double Sqr (double x);
 int main()
     {
     txCreateWindow (800, 600);
-    DrawGraphic(1, TX_LIGHTGREEN);
-    DrawGraphic(2, TX_LIGHTRED);
+    DrawGraphic(SIN, TX_LIGHTGREEN);
+    DrawGraphic(SQR, TX_LIGHTRED);
     //DrawGraphic(3);
     // DrawGraphic(4);
     }
@@ -20,10 +23,17 @@ void DrawGraphic (int func, COLORREF color)
     double x = -10;
     while (x<= +10)
         {
-        int y = Sqr(x);
-        if (func == 1) y = sin (x);
-        if (func == 2) y = Sqr (x);
-        txCircle (400+10*x, 300 - 10*y, 3);
+        double y = 0;
+        switch (func)
+            {
+            case SIN: y = sin (x);break;
+            case SQR: y = Sqr (x);break;
+
+            default: printf ("ERROR: DrawGraphic(): Invalid func %d\n", func);
+            }
+
+        txCircle (400+50*x, 300 - 50*y, 3);
+
         x += 0.01 ;
         }
     }
