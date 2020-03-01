03589fd08c72be7c6c761945300b967ab5e229d3
SusaninVodyanoy <61546634+SusaninVodyanoy@users.noreply.github.com>
2020-03-01 11:06:18 +0300

    изменение типов данных

--- KPK_6DAY/Primer.cpp
+++ KPK_6DAY/Primer.cpp
@@ -1,21 +1,27 @@
 #include "TXLib.h"
-const int SIN = 1,
-          SQR = 2;
+//const int SIN = 1,
+//          SQR = 2;
 
-void DrawGraphic (int func, COLORREF color);
+void DrawGraphic (double (*func) (double x), COLORREF color);
 double Sqr (double x);
+double sin2x (double x) ;
 
 
 int main()
     {
     txCreateWindow (800, 600);
-    DrawGraphic(SIN, TX_LIGHTGREEN);
-    DrawGraphic(SQR, TX_LIGHTRED);
+    DrawGraphic(&sin, TX_LIGHTGREEN);
+    DrawGraphic(&sin2x, TX_LIGHTGREEN);
+    DrawGraphic(&Sqr, TX_LIGHTRED);
     //DrawGraphic(3);
     // DrawGraphic(4);
     }
 
-void DrawGraphic (int func, COLORREF color)
+typedef unsigned long long superlong_t; // ассоциац€ выбраного имени с существующим типом
+
+typedef double (*func_t) (double x);
+
+void DrawGraphic (func_t func, COLORREF color)
     {
     txSetColor      (color);
     txSetFillColor  (color);
@@ -23,14 +29,14 @@ void DrawGraphic (int func, COLORREF color)
     double x = -10;
     while (x<= +10)
         {
-        double y = 0;
-        switch (func)
+        double y = (*func) (x);
+        /*switch (func)
             {
             case SIN: y = sin (x);break;
             case SQR: y = Sqr (x);break;
 
             default: printf ("ERROR: DrawGraphic(): Invalid func %d\n", func);
-            }
+            }*/
 
         txCircle (400+50*x, 300 - 50*y, 3);
 
@@ -39,3 +45,4 @@ void DrawGraphic (int func, COLORREF color)
     }
 
 double Sqr (double x) {return x*x;}
+double sin2x (double x) {return sin (2*x);}
